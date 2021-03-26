60e93af6eb8cd820be27a78de71606ebb85bc78b
shabobble <pksullivan@yahoo.com>
2021-03-26 17:27:34 -0400

    finished readme file

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# password-generator
+# Password Generator
+
+Third week's homework for UNH Full-Stack Web Development Boot Camp
+
+## Description
+
+The goal of this project was to modify some provided code to create a password generator application. Our main tasks were to ensure that:
+
+* Clicking the "Generate Password" button initiates a series of prompts for password criteria
+
+* These prompts allow the user to select which characters they would like including in their password
+
+* The user is prompted to provide a number for the length of their password between 8 and 128 characters in such a way that a number less than 8, greater than 128, or a non-number will not be accepted
+
+* When all prompts have been confirmed or denied, a password fitting the provided criteria is generated and printed into the text box on the screen
+
+## Built with
+
+* HTML
+* CSS
+* Javascript
+
+## Links
+
+* [GitHub Repository](https://github.com/shabobble/password-generator)
+* [Deployed application](https://shabobble.github.io/password-generator/)
+
+## Screenshots
+
+![Before Initiation](/assets/images/unstarted.jpg)
+![Length Prompt](/assets/images/length.jpg)
+![Example of Criteria Prompt](/assets/images/prompt.jpg)
+![Generated Password](/assets/images/password.jpg)
+
+## Installation
+
+No installations required for this project.
+
+## Credits
+
+My full-stack web development course is being offered as a collaboration between Trilogy Education Services and UNH Professional Development and Training.
+
+* Instructor - Benjamin Hutchins [GitHub](https://github.com/benhutchins)
+* TA - Casey Ferguson [LinkedIn](https://www.linkedin.com/in/casey-a-ferguson/)
--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/* Possible password characters */
-
-specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", "'", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "~"];
-number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-lowercaseAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-uppercaseAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -18,21 +9,24 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+
+  /* Possible password characters */
+
+let specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", "'", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "~"];
+let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let lowercaseAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let uppercaseAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 /* Defines variables */
 let confirmUppercase;
 let confirmLowercase;
 let confirmNumbers;
 let confirmCharacters;
-let selections;
-let pickSelections;
 
+/* Asks the user how many characters their password should be and ensures a number is entered */
 
-function generatePassword() {
-
-/* Asks the user how many characters their password should be */
-
-  passwordLength = parseInt(prompt("How many characters long should your password be? Choose a number between 8 and 128."));
+  let passwordLength = parseInt(prompt("How many characters long should your password be? Choose a number between 8 and 128."));
 
 /* Makes sure user enters a number between 8 and 128, else restarts the function */
 
@@ -47,12 +41,14 @@ function generatePassword() {
       else {
         alert("Thank you!");
           confirmUppercase = confirm("Should your password include uppercase letters?");
-          confirmLowercase = confirm("Should your password include lowercase letters?");
-          confirmNumbers = confirm("Should your password include numbers?");
-          confirmCharacters = confirm("Should your password include any special characters?");
+          confirmLowercase = confirm("Thank you, and should your password also include lowercase letters?");
+          confirmNumbers = confirm("Thank you, and should your password also include numbers?");
+          confirmCharacters = confirm("Final question: Should your password include any special characters?");
 
       }
     
+      let pickSelections;
+      let selections;
 
     if (!confirmUppercase && !confirmLowercase && !confirmNumbers && !confirmCharacters) {
       selections = alert("You must choose at least one criteria.");
